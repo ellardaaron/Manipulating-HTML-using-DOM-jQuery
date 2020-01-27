41d71831dd175037478f5f30d8ef6c2ae9b47d7c
Aaron Ellard <student@InnovateBhams-MacBook-Pro.local>
2020-01-27 16:52:19 -0600

    making progress?

--- DOM/app.js
+++ DOM/app.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //1
 
+    let button = document.createElement('button');
+    let btnText = document.createTextNode('This is a Button');
+    button.appendChild(btnText);
+    document.body.appendChild(button);
 
+    button.addEventListener("click", function () {
+        alert('any (nice) message')
+    });
+
+    //2
+
+    document.getElementById("btnSubmit").addEventListener("click", function (e) {
+        e.preventDefault();
+        var submitValue = document.getElementById("textInput").value;
+        alert(submitValue);
+    });
 })
+
 
